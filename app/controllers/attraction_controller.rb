308862be82ba85ca7ff3843fcec28325da6ff2b1
Ubuntu <ubuntu@ip-172-31-38-189.ec2.internal>
2022-11-23 09:03:01 +0000

    attractionコントローラーの作成(11/23未完成)

--- app/controllers/attraction_controller.rb
+++ app/controllers/attraction_controller.rb
@@ -2,4 +2,17 @@ class AttractionController < ApplicationController
     def index
         @attractions = Attraction.all
     end
+    
+    def new
+        @attraction = Attraction.new
+    end
+    
+    def create
+        @attraction = Attraction.new(name: params[:attraction][:name], prefecture: params[:attraction][:prefecture], 
+        text: params[:attraction][:text], url: params[:attraction][:url], genre: params[:attraction][:genre], 
+        open_time: params[:attraction][:open_time], contact: params[:attraction][:contact], 
+        user_id: params[:attraction][:user_id], time: params[:attraction][:time], address: params[:attraction][:address])
+        
+        redirect_to root_path
+    end
 end

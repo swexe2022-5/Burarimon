9e34e8ff95ab5ac895e1cfe1e63770afe377a304
Ubuntu <ubuntu@ip-172-31-65-76.ec2.internal>
2022-11-23 07:19:32 +0000

    a

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,10 @@
 class UserController < ApplicationController
+    def index
+        if session[:login_uid]
+            session.delete(:login_uid)
+            redirect_to root_path
+        else
+            render "login"
+        end
+    end
 end

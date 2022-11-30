e398c40d698b1e78f92f26397d0fae953ae48a9e
Matuzaki <ubuntu@ip-172-31-38-189.ec2.internal>
2022-11-30 09:00:22 +0000

    投稿ページ途中まで

--- app/controllers/attraction_controller.rb
+++ app/controllers/attraction_controller.rb
@@ -4,15 +4,17 @@ class AttractionController < ApplicationController
     end
     
     def new
+        @genres = ["レジャー", "文化・歴史", "自然", "芸術", "ショッピング", "温泉", "生き物"]
         @attraction = Attraction.new
     end
     
     def create
+        @genres = ["レジャー", "文化・歴史", "自然", "芸術", "ショッピング", "温泉", "生き物"]
         @attraction = Attraction.new(name: params[:attraction][:name], prefecture: params[:attraction][:prefecture], 
         text: params[:attraction][:text], url: params[:attraction][:url], genre: params[:attraction][:genre], 
-        open_time: params[:attraction][:open_time], contact: params[:attraction][:contact], 
-        user_id: params[:attraction][:user_id], time: params[:attraction][:time], address: params[:attraction][:address])
-        if 
+        open_time: params[:attraction][:open_time], contact: params[:attraction][:contact], user_id: params[:attraction][:user_id], 
+        time: params[:attraction][:time], address: params[:attraction][:address], time: Time.current)
+        if @attraction.save
             redirect_to root_path
         else
             render new_attraction_path

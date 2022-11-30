c8b71ab36206e46e2c82c0b139e857ad29bc2cbb
Ubuntu <ubuntu@ip-172-31-38-189.ec2.internal>
2022-11-30 06:30:33 +0000

    attractionコントローラーVer1完成

--- app/controllers/attraction_controller.rb
+++ app/controllers/attraction_controller.rb
@@ -12,7 +12,33 @@ class AttractionController < ApplicationController
         text: params[:attraction][:text], url: params[:attraction][:url], genre: params[:attraction][:genre], 
         open_time: params[:attraction][:open_time], contact: params[:attraction][:contact], 
         user_id: params[:attraction][:user_id], time: params[:attraction][:time], address: params[:attraction][:address])
-        
+        if 
+            redirect_to root_path
+        else
+            render new_attraction_path
+        end
+    end
+    
+    def show
+        @attraction = Attraction.find(params[:id])
+    end
+    
+    def edit
+        @attraction = Attraction.find(params[:id])
+    end
+    
+    def update
+        @attraction = Attraction.find(params[:id])
+        if @Tweet.update(message: params[:tweet][:message], image: params[:tweet][:image].read)
+            redirect_to root_path
+        else
+            render edit_attraction_path
+        end
+    end
+    
+    def destroy
+        @attraction = Attraction.find(params[:id])
+        @attraction.destroy
         redirect_to root_path
     end
 end

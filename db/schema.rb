70ce3ea9a819840a4ccb0028606b84e1727ff446
Matuzaki <ubuntu@ip-172-31-38-189.ec2.internal>
2023-01-13 10:19:46 +0000

    投稿画像最低限の機能完成

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2022_11_23_063917) do
     t.integer "user_id"
     t.datetime "time"
     t.string "address"
+    t.binary "picture1"
+    t.binary "picture2"
+    t.binary "picture3"
+    t.binary "picture4"
+    t.binary "picture5"
+    t.binary "picture6"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

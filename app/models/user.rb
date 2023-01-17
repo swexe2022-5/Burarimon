855cc0c7f2d978fb674b572ca0d3ff3bf488297e
Hanada <ubuntu@ip-172-31-65-76.ec2.internal>
2023-01-17 04:31:04 +0000

    validates修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :reviews
     validates :name, :pass, :profile, presence: true
-    validates :name, length: { maximum: 30 }
+    validates :name, length: { maximum: 30 }, uniqueness: true
     validates :password, confirmation: true
     
     attr_accessor :password, :password_confirmation
